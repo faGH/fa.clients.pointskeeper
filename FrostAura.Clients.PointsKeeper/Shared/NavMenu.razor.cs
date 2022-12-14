f52d2d3275405e0452a69f42c22c1690abeb32d2
Dean Martin <dean.martin@frostaura.net>
2022-12-14 11:02:54 +0200

    feat(Logo): Added a main logo to the menu bar and capabilities to manipulate the image.

--- FrostAura.Clients.PointsKeeper/Shared/NavMenu.razor.cs
+++ FrostAura.Clients.PointsKeeper/Shared/NavMenu.razor.cs
@@ -5,6 +5,7 @@ using FrostAura.Clients.PointsKeeper.Interfaces;
 using Microsoft.Extensions.Options;
 using FrostAura.Clients.PointsKeeper.Shared.Models;
 using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace FrostAura.Clients.PointsKeeper.Shared
 {
@@ -52,6 +53,14 @@ namespace FrostAura.Clients.PointsKeeper.Shared
             collapseNavMenu = !collapseNavMenu;
         }
 
+        private string GetMainLogoImgSrc()
+        {
+            var baseUrl = ApplicationConfigOptions.Value.AppBaseUrl;
+            var mainLogoUrl = $"{baseUrl}/img/main_logo.png";
+
+            return mainLogoUrl;
+        }
+
         private async Task SignInAsync()
         {
             var providedPinCode = await JsRuntime.InvokeAsync<string>("prompt", "Please enter your admin pin code.");
@@ -64,6 +73,20 @@ namespace FrostAura.Clients.PointsKeeper.Shared
         {
             await ClientDataStore.SetAsync(AUTH_TOKEN_KEY, string.Empty, CancellationToken.None);
             await OnAfterRenderAsync(false);
+        }
+
+        private async Task OnMainLogoChangedAsync(InputFileChangeEventArgs args)
+        {
+            if (!IsSignedIn) return;
+
+            var imagesDirectoryName = "wwwroot/img";
+            var baseUrl = Path.Combine(ApplicationConfigOptions.Value.AppBaseUrl, imagesDirectoryName);
+            var directory = Path.Combine(Directory.GetCurrentDirectory(), imagesDirectoryName);
+            var filename = $"{directory}/main_logo.png";
+
+            await using FileStream fs = new(filename, FileMode.Create);
+            await args.File.OpenReadStream(maxAllowedSize: int.MaxValue).CopyToAsync(fs);
+            await JsRuntime.InvokeVoidAsync("eval", "window.location.reload()");
         }
     }
 }
